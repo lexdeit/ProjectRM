863b6605f00a873b8a54b29d11c71415b67cee13
LEXDEIT <godhood@proton.me>
2023-02-21 23:09:52 -0600

    Actualizacion de funcionalidad de Generacion de Cards

--- src/Components/Card/CardU.jsx
+++ src/Components/Card/CardU.jsx
@@ -3,19 +3,16 @@ import styles from "../Card/CardU.module.css";
 
 export default function CardU(props) {
 
-    const quitarTarjeta = () => {
-        alert("Simulamos que se cierra")
-    }
-
     return (
         <>
             <div>
                 <div className={styles.card}>
                     <div className={styles.card2}>
+                        <button className={styles.btn} onClick={props.funcion}>❌</button>
                         <h2 className={styles.informacion}>Name: {props.name}</h2>
                         <h3 className={styles.texto}>Species: {props.species}</h3>
                         <h3 className={styles.texto}>Gender: {props.gender}</h3>
-                        <img className={styles.imagen} src={props.image} />
+                        <img className={styles.imagen} src={props.image} alt={props.name} />
                     </div>
                 </div>
             </div>
--- src/Components/Card/Cards.jsx
+++ src/Components/Card/Cards.jsx
@@ -1,18 +1,18 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, component } from "react";
 import CardU from "./CardU";
 import styles from "../Card/Cards.module.css";
 import Buttons from "../Buttons/Buttons";
-import { render } from "@testing-library/react";
 
 export default function Card() {
     const [tarjetasnext, settarjetanext] = useState([])
     const [tarjetasprev, settarjetaprev] = useState([])
     const [characters, setcharacter] = useState([]);
+    const [showCard, setShowCard] = useState(false);
 
     let urlCharacters = {
         url: 'https://rickandmortyapi.com/api/character',
-        data: []
+        data: [],
     };
 
     const axiosCharacters = (url) => {
@@ -22,6 +22,7 @@ export default function Card() {
                 settarjetanext(response.data.info.next);
                 settarjetaprev(response.data.info.prev);
             }).catch((error) => console.log(error))
+
     };
 
     useEffect(() => {
@@ -29,40 +30,65 @@ export default function Card() {
     }, [])
 
     const nuevasTarjetasnext = () => {
-        tarjetasnext.length >= 1 ? axiosCharacters?.(tarjetasnext) : console.log("No hay más tarjetas!");
+        tarjetasnext.length >= 1 ? axiosCharacters?.(tarjetasnext)
+            : console.log("No hay más tarjetas!");
     }
 
     const nuevasTarjetasprev = () => {
-        tarjetasprev.length >= 1 ? axiosCharacters?.(tarjetasprev) : console.log("No hay más tarjetas!");
+        tarjetasprev.length >= 1 ? axiosCharacters?.(tarjetasprev)
+            : console.log("No hay más tarjetas!");
     }
 
-    const renderizar = () => {
-
+    const cerrarTarjeta = () => {
+        showCard ? setShowCard(false)
+            : console.log("No hay mas tarjetas!");
     }
+    const renderizarf = () => {
+        setShowCard(true);
+    }
+
+    const randomCard = () => {
+        const randomNum = Math.floor(Math.random() * characters.length);
+        return (
+            <div
+            className={styles.tarjetas}>
+
+                <CardU
+                    name={characters[randomNum].name}
+                    gender={characters[randomNum].gender}
+                    species={characters[randomNum].species}
+                    image={characters[randomNum].image}
+                    funcion={cerrarTarjeta}
+                />
+
+            </div>
+        )
+    }
+
 
     return (
         <>
-
-            <div
-                className={styles.tarjetas}>
-                <CardU
-                    // name={characters[0].name}
-                    // gender={characters[0].gender}
-                    // species={characters[0].species}
-                    // image={characters[0].image}
-                />
-            </div>
-
             <div
                 className={styles.botonesr}>
 
+                <Buttons
+                    infoboton={"PREV"}
+                    funcion={nuevasTarjetasprev}>
+                </Buttons>
 
-                <Buttons infoboton={"PREV"} funcion={nuevasTarjetasprev}></Buttons>
-                <Buttons infoboton={"Random"} funcion={renderizar}></Buttons>
-                <Buttons infoboton={"NEXT"} funcion={nuevasTarjetasnext}></Buttons>
+                <Buttons
+                    infoboton={"Random"}
+                    funcion={renderizarf}>
+                </Buttons>
+
+                <Buttons
+                    infoboton={"NEXT"}
+                    funcion={nuevasTarjetasnext}>
+                </Buttons>
 
             </div>
-
+            
+            {showCard ? randomCard() : null}
         </>
     )
 }
