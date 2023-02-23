a14218d3a9439bb0b6b1cd38ebc7a90ee9a03ae4
LEXDEIT <godhood@proton.me>
2023-02-22 19:56:44 -0600

    Actualizacion grafica

--- src/Components/Card/CardU.jsx
+++ src/Components/Card/CardU.jsx
@@ -8,11 +8,11 @@ export default function CardU(props) {
             <div>
                 <div className={styles.card}>
                     <div className={styles.card2}>
-                        <button className={styles.btn} onClick={props.funcion}>❌</button>
-                        <h2 className={styles.informacion}>Name: {props.name}</h2>
+                        <button className={styles.btn} onClick={props.onClose}>❌</button>
+                        <h2 className={styles.informacion}>{props.name}</h2>
+                        <img className={styles.imagen} src={props.image} alt={props.name} />
                         <h3 className={styles.texto}>Species: {props.species}</h3>
                         <h3 className={styles.texto}>Gender: {props.gender}</h3>
-                        <img className={styles.imagen} src={props.image} alt={props.name} />
                     </div>
                 </div>
             </div>
--- src/Components/Card/Cards.jsx
+++ src/Components/Card/Cards.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useEffect, useState, component } from "react";
+import React, { useEffect, useState } from "react";
 import CardU from "./CardU";
 import styles from "../Card/Cards.module.css";
 import Buttons from "../Buttons/Buttons";
@@ -9,10 +9,6 @@ export default function Card() {
     const [showCard, setShowCard] = useState([]);
     const [cards, setcards] = useState([])
 
-    let urlCharacters = {
-        url: 'https://rickandmortyapi.com/api/character',
-        data: [],
-    };
 
     const axiosCharacters = (url) => {
         axios.get(url)
@@ -21,9 +17,9 @@ export default function Card() {
                 let nuevosCaracteres = response.data.results;
                 setcharacter(prevCharacters => [...prevCharacters, ...nuevosCaracteres]);
 
-                response.data.info.next ? 
-                axiosCharacters(response.data.info.next) 
-                : console.log("No hay mas paginas!");
+                response.data.info.next ?
+                    axiosCharacters(response.data.info.next)
+                    : console.log("No hay mas paginas!");
 
             })
             .catch((error) => console.log(error));
@@ -32,30 +28,32 @@ export default function Card() {
 
 
     useEffect(() => {
-        axiosCharacters(urlCharacters.url);
+        axiosCharacters('https://rickandmortyapi.com/api/character');
     }, [])
 
 
     const nuevasCardU = () => {
         const tarjetasPersonaje = characters.map((personaje) => (
             <CardU
-                key={personaje.id}
-                name={personaje.name}
-                gender={personaje.gender}
-                species={personaje.species}
-                image={personaje.image}
-                funcion={() => cerrarTarjeta(personaje.id)} // pass the key to the function
+              key={personaje.id}
+              name={personaje.name}
+              gender={personaje.gender}
+              species={personaje.species}
+              image={personaje.image}
+              funcion={setShowCard()}
             />
-
-        ))
+          ));
+          
         setcards(tarjetasPersonaje)
     }
 
 
-    const cerrarTarjeta = (key) => {
-        const filtrados = showCard.filter((elemento) => elemento.key !== key)
-        setShowCard(filtrados)
-    }
+    const cerrarTarjeta = () => {
+        setShowCard([])
+      };
+      
+      
+      
 
     const randomCard = () => {
         nuevasCardU();
@@ -65,7 +63,9 @@ export default function Card() {
     };
 
     const mostrarinfo = () => {
-        console.log(characters);
+        for (let i = 0; i < showCard.length; i++) {
+            console.log(showCard);
+        }
     }
 
     return (
@@ -75,7 +75,7 @@ export default function Card() {
 
                 <Buttons
                     infoboton={"PREV"}
-                    >
+                >
                 </Buttons>
 
                 <Buttons
@@ -85,7 +85,7 @@ export default function Card() {
 
                 <Buttons
                     infoboton={"NEXT"}
-                    >
+                >
                 </Buttons>
 
                 <Buttons
