1e8c9e7d05783824af338d21ec9571fc85b4497c
LEXDEIT <115962817+lexdeit@users.noreply.github.com>
2023-03-11 10:06:38 -0600

    Actualizacion de UI

--- src/Components/Details/CardDetails.jsx
+++ src/Components/Details/CardDetails.jsx
@@ -1,28 +1,38 @@
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import styles from './CardDetails.module.css';
 
 const CardDetails = () => {
     const { id } = useParams();
     const characters = useSelector((state) => state.characters);
-    const { name, image, species, gender, origin, status, type } = characters.find((character) => `${character.id}` === `${id}`);
+    const { name, image, species, gender, origin, status, type } =
+        characters.find((character) => `${character.id}` === `${id}`);
 
     return (
         <>
 
-            <iframe src="https://www.youtube.com/embed/6vAxc0Av_UI?&autoplay=1&loop=1&mute=1&showinfo=0&controls=0"></iframe>
+            <iframe src="https://www.youtube.com/embed/6vAxc0Av_UI?&autoplay=1&loop=1&mute=1&showinfo=0&controls=0" />
             <div className={styles.content}>
                 <div className={styles.container}>
-                    <h2 className={styles.informacion}>{name}</h2>
 
-                    <img className={styles.imagen} src={image} alt={name} />
 
-                    <h3 className={styles.texto}>Species: {species}</h3>
 
-                    <h3 className={styles.texto}>Gender: {gender}</h3>
-                    <h3 className={styles.texto}>Origin: {origin.name}</h3>
-                    <h3 className={styles.texto}>Status: {status}</h3>
-                    <h3 className={styles.texto}>Type: {type}</h3>
+                    
+                    <div>
+
+                        <img className={styles.imagen} src={image} alt={name} />
+                    </div>
+
+
+                    <div>
+                        <h2 className={styles.informacion}>{name}</h2>
+                        <h3 className={styles.texto}>Species: {species}</h3>
+                        <h3 className={styles.texto}>Gender: {gender}</h3>
+                        <h3 className={styles.texto}>Origin: {origin.name}</h3>
+                        <h3 className={styles.texto}>Status: {status}</h3>
+                        <h3 className={styles.texto}>Type: {type}</h3>
+                    </div>
                 </div>
             </div>
 
