0d3fcbe06d9e809256c30202c620436a09fd4d86
LEXDEIT <godhood@proton.me>
2023-03-13 10:35:57 -0600

    Update F&UI

--- src/View/Landing.jsx
+++ src/View/Landing.jsx
@@ -5,7 +5,7 @@ import { useDispatch } from "react-redux";
 import { getApi } from "../Redux/Actions";
 import { useSelector } from 'react-redux';
 import Buttons from '../Components/Buttons/Buttons';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 
 const Landing = () => {
@@ -13,8 +13,7 @@ const Landing = () => {
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
     const [sucess, setSucess] = useState("");
-
-
+    const navigate = useNavigate();
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(getApi('https://rickandmortyapi.com/api/character'));
@@ -38,7 +37,7 @@ const Landing = () => {
             <iframe src="https://www.youtube.com/embed/Jgbp9A7onSc?&autoplay=1&loop=1&mute=1&showinfo=0&controls=0"></iframe>
 
             <div className={styles.content} >
-                {characters.length > 1000 ?
+                {characters.length !== 0 ?
                     <>
                         <div className={styles.loginbox}>
                             <img src='logo.png' className={styles.logo} />
@@ -70,8 +69,8 @@ const Landing = () => {
                                 />
 
                             </form>
-                            {error && <p className={styles.error}>Error</p>}
-                            {sucess && <Link to={"/home"}></Link> }
+                            {error && <p className={styles.error}>Please enter your email and password</p>}
+                            {sucess && navigate('/home')}
 
                         </div>
                     </>
