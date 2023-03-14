04c4bf3147d32e919c092abe8b652475275915de
LEXDEIT <godhood@proton.me>
2023-03-14 15:49:25 -0600

    Responsive Design

--- src/View/About/Aboutview.jsx
+++ src/View/About/Aboutview.jsx
@@ -1,11 +1,13 @@
 import About from "../../Components/About/About"
 import MenuHome from "../../Components/Menu/Menu"
+import Mobilemenu from "../../Components/Menu/MobileMenu";
 
 const Aboutview = () => {
     return (
         <>
         <MenuHome/>
         <About/>
+        <Mobilemenu/>
         </>
     )
 }
