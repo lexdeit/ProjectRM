5a7afce99751bab0caeaa9d2e10b9b862d5e31a0
LEXDEIT <godhood@proton.me>
2023-03-26 21:56:12 -0600

    New functions!

--- Back-End/controllers/getCharById.js
+++ Back-End/controllers/getCharById.js
@@ -1,11 +1,33 @@
-import axios from "axios"
+const axios = require("axios");
+const URL = `https://rickandmortyapi.com/api/character/`;
 
+
+
+
+//Funcion que maneja en caso de exito!
+const succesHandler = (response, res) => {
+    const { id, image, name, gender, species } = response.data;
+    res.WriteHead(200, { "Content-Type": "application/json" })
+        && res.end(JSON.stringify({ id, image, name, gender, species }));
+};
+
+
+//Funcion que maneja en caso de error!
+const errorHandler = (err, res) => {
+    res.WriteHead(500, { "Content-Type": "text/plain" })
+        && res.end(`${err.message}`);
+};
+
+//Funcion raiz que realiza la peticion y los llamados a la funcion exitosa o funcion de error!
 const getCharById = (res, ID) => {
-    axios.get(`https://rickandmortyapi.com/api/character/${ID}`)
-    .then(response => response.data)
-    .then(data => )
-}
+
+    axios
+        .get(`${URL}${ID}`)
+        .then(response => succesHandler(response, res))
+        .catch(err => errorHandler(err, res));
+
+};
 
 module.exports = {
     getCharById,
-}
+};
--- Back-End/controllers/getCharDetail.js
+++ Back-End/controllers/getCharDetail.js
@@ -1,7 +1,28 @@
+const axios = require("axios");
+const URL = `https://rickandmortyapi.com/api/character/`;
+
+const succesHandler = (response, res) => {
+  const { image, name, gender, status, origin, species } = response.data;
+  res.writeHead(200, { "Content-Type": "application/json" })
+    && res.end(JSON.stringify({ image, name, gender, status, origin, species }));
+}
+
+const errorHandler = (err, res) => {
+  res.writeHead(500, { "Content-Type": "text/plain" })
+    && res.end(`${err.message}`)
+}
+
+
+
 const getCharDetail = (res, ID) => {
+
+  axios
+    .get(`${URL}${ID}`)
+    .then(response => succesHandler(response, res))
+    .catch(err => errorHandler(err, res))
 
 }
 
 module.exports = {
-    getCharDetail,
-  }
+  getCharDetail,
+}
